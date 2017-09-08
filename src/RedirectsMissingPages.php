a511dc9a92be3dc295cdb128c44743c92f2c5ab1
d0m <d0m@d0m>
2017-09-08 16:56:30 +0100

    Updated Readme Renamed option "status_code" to "redirect_status_codes" Updated tests

--- src/RedirectsMissingPages.php
+++ src/RedirectsMissingPages.php
@@ -12,8 +12,8 @@ class RedirectsMissingPages
     {
         $response = $next($request);
 
-        $allowedStatusCodes = config('missing-page-redirector.redirect_status_codes', [Response::HTTP_NOT_FOUND]);
-
+        $allowedStatusCodes = config('missing-page-redirector.redirect_status_codes');
+        
         //If option is set as null or the response status code is not present in the config then skip redirects
         if (is_null($allowedStatusCodes) || (!empty($allowedStatusCodes) && !in_array($response->getStatusCode(), $allowedStatusCodes))) {
             return $response;

c56239a17f73fcef4290bd04fd71bcb3c7d524dc
Alexandru Manase <malexandru699@gmail.com>
2022-11-11 13:32:08 +0200

    Refactor tests to Pest (#79)
    * refactor `RedirectsMissingPagesTest`
    
    * cleanup
    
    * cleanup
    
    * replace test suite in Github action and `composer.json`
    
    * Fix styling
    
    * remove useless laravel pest plugin
    
    * up requirements
    
    Co-authored-by: alexmanase <alexmanase@users.noreply.github.com>

--- tests/RedirectsMissingPagesTest.php
+++ tests/RedirectsMissingPagesTest.php
@@ -1,193 +1,145 @@
 <?php
 
-namespace Spatie\MissingPageRedirector\Test;
-
 use Illuminate\Support\Facades\Event;
 use Spatie\MissingPageRedirector\Events\RedirectNotFound;
 use Spatie\MissingPageRedirector\Events\RouteWasHit;
 use Symfony\Component\HttpFoundation\Response;
 
-class RedirectsMissingPagesTest extends TestCase
-{
-    /** @test */
-    public function it_will_not_interfere_with_existing_pages()
-    {
-        $this
-            ->get('existing-page')
-            ->assertSee('existing page');
-    }
+it('will not interfere with existing pages')
+    ->get('existing-page')
+    ->assertSee('existing page');
 
-    /** @test */
-    public function it_will_redirect_a_non_existing_page_with_a_permanent_redirect()
-    {
-        $this->app['config']->set('missing-page-redirector.redirects', [
+it('will redirect a non existing page with a permanent redirect')
+    ->tap(
+        fn () => config()->set('missing-page-redirector.redirects', [
             '/non-existing-page' => '/existing-page',
-        ]);
+        ])
+    )
+    ->get('non-existing-page')
+    ->assertStatus(Response::HTTP_MOVED_PERMANENTLY)
+    ->assertRedirect('/existing-page');
 
-        $this
-            ->get('non-existing-page')
-            ->assertStatus(Response::HTTP_MOVED_PERMANENTLY)
-            ->assertRedirect('/existing-page');
-    }
+it('will redirect wildcard routes')
+    ->tap(fn () => config()->set('missing-page-redirector.redirects', [
+        '/path/*' => '/new-path/{wildcard}',
+    ]))
+    ->get('path/to/a/page')
+    ->assertStatus(Response::HTTP_MOVED_PERMANENTLY)
+    ->assertRedirect('/new-path/to/a/page');
 
-    /** @test */
-    public function it_will_redirect_wildcard_routes()
-    {
-        $this->app['config']->set('missing-page-redirector.redirects', [
-            '/path/*' => '/new-path/{wildcard}',
-        ]);
-
-        $this
-            ->get('path/to/a/page')
-            ->assertStatus(Response::HTTP_MOVED_PERMANENTLY)
-            ->assertRedirect('/new-path/to/a/page');
-    }
-
-    /** @test */
-    public function it_will_not_redirect_an_url_that_it_not_configured()
-    {
-        $this->app['config']->set('missing-page-redirector.redirects', [
+it('will not redirect an url that is not configured')
+    ->tap(
+        fn () => config()->set('missing-page-redirector.redirects', [
             '/non-existing-page' => '/existing-page',
-        ]);
+        ])
+    )
+    ->get('/not-configured')
+    ->assertStatus(Response::HTTP_NOT_FOUND);
 
-        $this
-            ->get('/not-configured')
-            ->assertStatus(Response::HTTP_NOT_FOUND);
-    }
-
-    /** @test */
-    public function it_can_use_named_parameters()
-    {
-        $this->app['config']->set('missing-page-redirector.redirects', [
+it('can use named properties')
+    ->tap(
+        fn () => config()->set('missing-page-redirector.redirects', [
             '/segment1/{id}/segment2/{slug}' => '/segment2/{slug}',
-        ]);
+        ])
+    )
+    ->get('/segment1/123/segment2/abc')
+    ->assertRedirect('/segment2/abc');
 
-        $this
-            ->get('/segment1/123/segment2/abc')
-            ->assertRedirect('/segment2/abc');
-    }
 
-    /** @test */
-    public function it_can_use_multiple_named_parameters_in_one_segment()
-    {
-        $this->app['config']->set('missing-page-redirector.redirects', [
+it('can use (multiple named parameters in one segment')
+    ->tap(
+        fn () => config()->set('missing-page-redirector.redirects', [
             '/new-segment/{id}-{slug}' => '/new-segment/{id}/',
-        ]);
+        ])
+    )
+    ->get('/new-segment/123-blablabla')
+    ->assertRedirect('/new-segment/123');
 
-        $this
-            ->get('/new-segment/123-blablabla')
-            ->assertRedirect('/new-segment/123');
-    }
-
-    /** @test */
-    public function it_can_optionally_set_the_redirect_status_code()
-    {
-        $this->app['config']->set('missing-page-redirector.redirects', [
+it('can optionally set the redirect status code')
+    ->tap(
+        fn () => config()->set('missing-page-redirector.redirects', [
             '/temporarily-moved' => ['/just-for-now', 302],
-        ]);
+        ])
+    )
+    ->get('/temporarily-moved')
+    ->assertStatus(302)
+    ->assertRedirect('/just-for-now');
 
-        $this
-            ->get('/temporarily-moved')
-            ->assertStatus(302)
-            ->assertRedirect('/just-for-now');
-    }
+it('can use optional parameters', function (string $getRoute, string $redirectRoute) {
+    config()->set('missing-page-redirector.redirects', [
+        '/old-segment/{parameter1?}/{parameter2?}' => '/new-segment/{parameter1}/{parameter2}',
+    ]);
 
-    /** @test */
-    public function it_can_use_optional_parameters()
-    {
-        $this->app['config']->set('missing-page-redirector.redirects', [
-            '/old-segment/{parameter1?}/{parameter2?}' => '/new-segment/{parameter1}/{parameter2}',
-        ]);
+    $this->get($getRoute)
+        ->assertRedirect($redirectRoute);
+})->with([
+    ['/old-segment', '/new-segment'],
+    ['/old-segment/old-segment2', '/new-segment/old-segment2'],
+    ['/old-segment/old-segment2/old-segment3', '/new-segment/old-segment2/old-segment3'],
+]);
 
-        $this
-            ->get('/old-segment')
-            ->assertRedirect('/new-segment');
+test('by default it will not redirect requests that are nit 404s')
+    ->get('/response-code/500')
+    ->assertStatus(500);
 
-        $this
-            ->get('/old-segment/old-segment2')
-            ->assertRedirect('/new-segment/old-segment2');
+it('will fire an event when a route is hit', function () {
+    Event::fake();
 
-        $this
-            ->get('/old-segment/old-segment2/old-segment3')
-            ->assertRedirect('/new-segment/old-segment2/old-segment3');
-    }
+    $this->app['config']->set('missing-page-redirector.redirects', [
+        '/old-segment/{parameter1?}/{parameter2?}' => '/new-segment/',
+    ]);
 
-    /** @test */
-    public function by_default_it_will_not_redirect_requests_that_are_not_404s()
-    {
-        $this
-            ->get('/response-code/500')
-            ->assertStatus(500);
-    }
+    $this->get('/old-segment');
 
-    /** @test */
-    public function it_will_fire_an_event_when_a_route_is_hit()
-    {
-        Event::fake();
+    Event::assertDispatched(RouteWasHit::class);
+});
 
-        $this->app['config']->set('missing-page-redirector.redirects', [
-            '/old-segment/{parameter1?}/{parameter2?}' => '/new-segment/',
-        ]);
-
-        $this->get('/old-segment');
-
-        Event::assertDispatched(RouteWasHit::class);
-    }
-
-    /** @test */
-    public function it_will_redirect_depending_on_redirect_status_codes_defined()
-    {
-        $this->app['config']->set('missing-page-redirector.redirect_status_codes', [
-            418,
-            500,
-        ]);
-
-        $this->app['config']->set('missing-page-redirector.redirects', [
+it('will redirect depending on redirect status code defined')
+    ->tap(
+        fn () => config()->set(
+            'missing-page-redirector.redirect_status_codes',
+            [418, 500]
+        )
+    )
+    ->tap(
+        fn () => config()->set('missing-page-redirector.redirects', [
             '/response-code/500' => '/existing-page',
-        ]);
+        ])
+    )
+    ->get('/response-code/500')
+    ->assertRedirect('/existing-page');
 
-        $this
-            ->get('/response-code/500')
-            ->assertRedirect('/existing-page');
-    }
-
-    /** @test */
-    public function it_will_not_redirect_if_the_status_code_is_not_specified_in_the_config_file()
-    {
-        $this->app['config']->set('missing-page-redirector.redirect_status_codes', [
-            418,
-            403,
-        ]);
-
-        $this->app['config']->set('missing-page-redirector.redirects', [
+it('will not redirect if the status code is not specified in the config file')
+    ->tap(
+        fn () => config()->set(
+            'missing-page-redirector.redirect_status_codes',
+            [418, 403]
+        )
+    )
+    ->tap(
+        fn () => config()->set('missing-page-redirector.redirects', [
             '/response-code/500' => '/existing-page',
-        ]);
+        ])
+    )
+    ->get('/response-code/500')
+    ->assertStatus(500);
 
-        $this
-            ->get('/response-code/500')
-            ->assertStatus(500);
-    }
-
-    /** @test */
-    public function it_will_redirect_on_any_status_code()
-    {
-        $this->app['config']->set('missing-page-redirector.redirect_status_codes', []);
-        $this->app['config']->set('missing-page-redirector.redirects', [
+it('will redirect on any status code')
+    ->tap(
+        fn () => config()->set('missing-page-redirector.redirect_status_codes', [])
+    )
+    ->tap(
+        fn () => config()->set('missing-page-redirector.redirects', [
             '/response-code/418' => '/existing-page',
-        ]);
+        ])
+    )
+    ->get('/response-code/418')
+    ->assertRedirect('/existing-page');
 
-        $this
-            ->get('/response-code/418')
-            ->assertRedirect('/existing-page');
-    }
+it('will fire an event when no redirect was found', function () {
+    Event::fake();
 
-    /** @test */
-    public function it_will_fire_an_event_when_no_redirect_was_found()
-    {
-        Event::fake();
+    $this->get('/response-code/404');
 
-        $this->get('/response-code/404');
-
-        Event::assertDispatched(RedirectNotFound::class);
-    }
-}
+    Event::assertDispatched(RedirectNotFound::class);
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,7 +11,7 @@ use Spatie\MissingPageRedirector\RedirectsMissingPages;
 
 abstract class TestCase extends Orchestra
 {
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
